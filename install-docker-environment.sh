0ce26481661c496a1b9b2cf771539bfeacff46a8
TECHN0ID <nichham2@hotmail.com>
2023-10-11 19:28:41 -0500

    Update install-docker-environment.sh
    Moved -y in the install command for docker

--- install-docker-environment.sh
+++ install-docker-environment.sh
@@ -38,11 +38,11 @@ if [ -x "$(command -v dnf)" ]; then
 # Distrib Debian / Ubuntu
 elif [ -x "$(command -v apt-get)" ]; then
     apt update && apt upgrade -y
-    apt install apt-transport-https ca-certificates curl software-properties-common
+    apt install apt-transport-https ca-certificates curl software-properties-common -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update -y
-    apt-get -y install docker-ce docker-compose
+    apt-get install docker-ce docker-compose -y
 
 else
     echo "❌  This script is not compatible with your system"
